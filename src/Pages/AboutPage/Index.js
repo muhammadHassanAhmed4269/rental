06da44a2cf71d93d2e1cb4d286389c29a22daf91
wellashirr <ashirshoaib2001@gmail.com>
2023-12-12 23:36:34 +0500

    new

--- src/Pages/AboutPage/Index.js
+++ src/Pages/AboutPage/Index.js
@@ -2,6 +2,9 @@ import React from "react";
 import AboutBanner from "../../Assets/AboutBanner.png";
 import Navbar from "../../Components/Navbar/index";
 import Footer from "../../Components/Footer/Index";
+import AboutImg1 from "../../Assets/about1.png";
+import AboutImg2 from "../../Assets/about2.png";
+import AboutImg3 from "../../Assets/about3.png";
 const Index = () => {
   return (
     <div>
@@ -26,50 +29,62 @@ const Index = () => {
         </div>
       </div>
       <div className="w-11/12 mb-10 mx-auto">
-        <div className="mt-16">
-          <h1 className="font-bold text-xl">
-            Welcome to Lend Anything, your one-stop shop for borrowing and
-            lending anything!
-          </h1>
-          <p className="mt-10 text-xl">
-            We believe that sharing resources is essential for building a more
-            sustainable and equitable world. That's why we've <br /> created
-            LendAnything, a platform that connects people who have things they
-            don't need with people who need those <br /> things
-          </p>
+        <div className="mt-16 flex justify-between">
+          <div className="w-4/5">
+            <h1 className="font-bold text-xl">
+              Welcome to Lend Anything, your one-stop shop for borrowing and
+              lending <br />
+              anything!
+            </h1>
+            <p className="mt-10 text-xl">
+              We believe that sharing resources is essential for building a more
+              <br />
+              sustainable and equitable world. That's why we've created
+              LendAnything, a <br /> platform that connects people who have
+              things they don't need with <br /> people who need those things
+            </p>
+          </div>
+          <img src={AboutImg1} alt="" />
         </div>
-        <div className="mt-16">
-          <h1 className="font-bold text-xl">How does it work?</h1>
-          <p className="mt-10 text-xl">
-            To lend an item, simply create a listing on our website. Be sure to
-            include a description of the item, photos, and your <br />{" "}
-            availability. Once your listing is active, other users will be able
-            to browse and borrow it. <br /> To borrow an item, simply search for
-            what you need on our website. Once you've found an item you're
-            interested in, contact <br /> the lender to arrange the borrowing
-          </p>
+        <div className="mt-16 flex">
+          <div>
+            <h1 className="font-bold text-xl">How does it work?</h1>
+            <p className="mt-10 text-xl">
+              To lend an item, simply create a listing on our website. Be sure
+              to include a description of the item, photos, and your <br />{" "}
+              availability. Once your listing is active, other users will be
+              able to browse and borrow it. <br /> To borrow an item, simply
+              search for what you need on our website. Once you've found an item
+              you're interested in, contact <br /> the lender to arrange the
+              borrowing
+            </p>
+          </div>
+          <img src={AboutImg2} alt="" />
         </div>
-        <div className="mt-16">
-          <h1 className="font-bold text-xl">
-            Why should you use LendAnything?
-          </h1>
-          <p className="mt-10 text-xl">
-            There are many benefits to using LendAnything, including:
-            <ol>
-              <li className="list-disc ml-8">
-                Save money: Borrowing items instead of buying them can save you
-                a lot of money.
-              </li>
-              <li className="list-disc ml-8">
-                Reduce waste: By sharing resources, we can help to reduce the
-                amount of waste going to landfills.
-              </li>
-              <li className="list-disc ml-8">
-                Build community: LendAnything is a great way to connect with
-                your neighbors and build a sense of community.
-              </li>
-            </ol>
-          </p>
+        <div className="mt-16 flex">
+          <div>
+            <h1 className="font-bold text-xl">
+              Why should you use LendAnything?
+            </h1>
+            <p className="mt-10 text-xl">
+              There are many benefits to using LendAnything, including:
+              <ol>
+                <li className="list-disc ml-8">
+                  Save money: Borrowing items instead of buying them can save
+                  you a lot of money.
+                </li>
+                <li className="list-disc ml-8">
+                  Reduce waste: By sharing resources, we can help to reduce the
+                  amount of waste going to landfills.
+                </li>
+                <li className="list-disc ml-8">
+                  Build community: LendAnything is a great way to connect with
+                  your neighbors and build a sense of community.
+                </li>
+              </ol>
+            </p>
+          </div>
+          <img src={AboutImg3} alt="" />
         </div>
       </div>
       <Footer />
